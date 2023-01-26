1b89941e8ee712f85d9a885c0fc0844619acd7f3
salvadorf8 <aztecsalvador@gmail.com>
2023-01-26 08:10:12 -0600

    Panel Component | EXCELLENT sample use of a Panel Component - Reusable "Presentation" Component - when duplicate tailwindcss are duplicating everywhere, you can use a panel to clean things up

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { GoChevronDown } from 'react-icons/go';
+import Panel from './Panel';
 
 const Dropdown = ({ options, onChange, value }) => {
     const [isOpen, setIsOpen] = useState(false);
@@ -24,11 +25,11 @@ const Dropdown = ({ options, onChange, value }) => {
 
     return (
         <div className='w-48 relative'>
-            <div className='flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full' onClick={handleOnClick}>
+            <Panel className='flex justify-between items-center cursor-pointer' onClick={handleOnClick}>
                 {value?.label || 'Select...'}
                 <GoChevronDown className='text-lg' />
-            </div>
-            {isOpen && <div className='absolute top-full border rounded p-3 shadow bg-white w-full'>{renderedOptions}</div>}
+            </Panel>
+            {isOpen && <Panel className='absolute top-full'>{renderedOptions}</Panel>}
         </div>
     );
 };
