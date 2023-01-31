e8a14ff77c6096d293852d64b934fcb6f5a78d9a
salvadorf8 <aztecsalvador@gmail.com>
2023-01-31 17:58:13 -0600

    Skeleton Loader | EXCELLENT NOTE!!!!! good coding to create a skeleton loader to show user items are loading from an API call

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store';
+import Skeleton from './Skeleton';
 
 const UsersList = () => {
     const dispatch = useDispatch();
@@ -14,7 +15,7 @@ const UsersList = () => {
     }, []);
 
     if (isLoading) {
-        return <div>Loading...</div>;
+        return <Skeleton times={6} className='h-10 w-full' />;
     }
 
     if (error) {
--- media/tailwind.config.js
+++ media/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            keyframes: {
+                shimmer: {
+                    '100%': { transform: 'translateX(100%)' }
+                }
+            },
+            animation: {
+                shimmer: 'shimmer 2s infinite'
+            }
+        }
     },
     plugins: []
 };
+
+/**
+ * Key frames are what are used to actually implement an animation, in this case,
+ * we're saying move an element 100% to the right hand side
+ * second animation says go and find the shimmer rule that we just set up, and play it over 1.5 seconds, repeat, infinite
+ */
