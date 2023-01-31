b61723027771b11ba110c35033df0c445773c28d
salvadorf8 <aztecsalvador@gmail.com>
2023-01-31 16:25:37 -0600

    AsyncThunk | updated some code to test

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,15 +1,27 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store';
 
 const UsersList = () => {
     const dispatch = useDispatch();
+    // REMEMBER: state.users will have { data: [], isLoading: false, error: null }
+    const { isLoading, data, error } = useSelector((state) => {
+        return state.users;
+    });
 
     useEffect(() => {
         dispatch(fetchUsers());
     }, []);
 
-    return <div>UsersList</div>;
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error fetching data...</div>;
+    }
+
+    return <div>{data.length}</div>;
 };
 
 export default UsersList;
