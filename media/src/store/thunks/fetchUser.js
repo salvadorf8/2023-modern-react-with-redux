49df60d0b1e85fb3b1b2ea7f4d69e0d949e697a1
salvadorf8 <aztecsalvador@gmail.com>
2023-01-31 16:28:27 -0600

    testing Loading | EXCELLENT example to slow down the application to test the loading functionality an extra second

--- media/src/store/thunks/fetchUser.js
+++ media/src/store/thunks/fetchUser.js
@@ -6,8 +6,18 @@ import axios from 'axios';
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
     const response = await axios.get('http://localhost:3005/users');
 
+    // DEV ONLY!!!
+    await pause(1000);
+
     return response.data;
 });
+
+// DEV ONLY!!!! to add a pause for loading
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    });
+};
 
 export { fetchUsers };
 
