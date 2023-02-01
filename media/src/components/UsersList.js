3ef11639e957a788bee5952a0e3cd7352ca4e8d6
salvadorf8 <aztecsalvador@gmail.com>
2023-01-31 22:00:35 -0600

    Moved some thunk to useState | EXCELLENT in the case you need to do some special technique to render loaders or errors for the button REMEMBER had to use unwrap()

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { fetchUsers, addUser } from '../store';
@@ -6,25 +6,46 @@ import Button from './Button';
 import Skeleton from './Skeleton';
 
 const UsersList = () => {
+    const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+    const [loadingUsersError, setLoadingUsersError] = useState(null);
+
+    const [isCreatingUser, setIsCreatingUser] = useState(false);
+    const [creatingUserError, setCreatingUserError] = useState(null);
     const dispatch = useDispatch();
     // REMEMBER: state.users will have { data: [], isLoading: false, error: null }
-    const { isLoading, data, error } = useSelector((state) => {
+    const { data } = useSelector((state) => {
         return state.users;
     });
 
     useEffect(() => {
-        dispatch(fetchUsers());
+        setIsLoadingUsers(true);
+        dispatch(fetchUsers())
+            .unwrap()
+            .then(() => {
+                // setIsLoadingUser(false); //comented because of finally
+            })
+            .catch((error) => {
+                console.log('SF - error', error);
+                setLoadingUsersError(error);
+            })
+            .finally(() => {
+                setIsLoadingUsers(false);
+            });
     }, []);
 
     const handleUserAdd = () => {
-        dispatch(addUser());
+        setIsCreatingUser(true);
+        dispatch(addUser())
+            .unwrap()
+            .catch((error) => setCreatingUserError(error))
+            .finally(() => setIsCreatingUser(false));
     };
 
-    if (isLoading) {
+    if (isLoadingUsers) {
         return <Skeleton times={6} className='h-10 w-full' />;
     }
 
-    if (error) {
+    if (loadingUsersError) {
         return <div>Error fetching data...</div>;
     }
 
@@ -40,7 +61,8 @@ const UsersList = () => {
         <div>
             <div className='flex flex-row justify-between m-3'>
                 <h1 className='m-2 text-xl'>Users</h1>
-                <Button onClick={handleUserAdd}>+ Add User</Button>
+                {isCreatingUser ? 'CreatingUser...' : <Button onClick={handleUserAdd}>+ Add User</Button>}
+                {creatingUserError && 'Error creating User...'}
             </div>
             {renderedUsers}
         </div>
